612755ecf1bf838c07fe6c9fa062ccc7ae3015f3
CezarJedi <cezar.jedimaster@gmail.com>
2026-05-22 19:39:20 +0300

    Added better quality support

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import yt_dlp # pip install yt-dlp
 url = input("Enter YouTube URL: ")
 
 ydl_opts = {
-    'format': 'best[ext=mp4]/best', 
+    # 'format': 'best[ext=mp4]/best', lower quality
+    'format': 'bestvideo+bestaudio/best',
+    'merge_output_format': 'mp4',
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
