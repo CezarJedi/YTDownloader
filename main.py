aa3987130b6f64a378dcbcac1e127978a8bfc1da
CezarJedi <cezar.jedimaster@gmail.com>
2026-05-22 19:51:45 +0300

    Better fps

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import yt_dlp # pip install yt-dlp
 url = input("Enter YouTube URL: ")
 
 ydl_opts = {
-    # 'format': 'best[ext=mp4]/best', lower quality
-    'format': 'bestvideo+bestaudio/best',
+    # 'format': 'best[ext=mp4]/best', # lower quality
+    'format': 'bestvideo[fps=60]+bestaudio/bestvideo+bestaudio/best',
+    'format_sort': ['fps', 'res'], # prioritizes fps over resolution
     'merge_output_format': 'mp4',
 }
 
